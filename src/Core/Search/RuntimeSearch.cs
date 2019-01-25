12b6ac4b90f6c9bca7111bd3600f4011238eed5d
Richard Griffiths <cwolfs@gmail.com>
2019-01-25 13:38:14 +0100

    Initial search code work

--- src/Core/Search/RuntimeSearch.cs
+++ src/Core/Search/RuntimeSearch.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
+
+using System.Reflection;
 
 namespace BTDebug.RuntimeSearch {
 	public class RuntimeSearch : MonoBehaviour {
@@ -18,7 +22,20 @@ namespace BTDebug.RuntimeSearch {
     }
 
     public static void Search(string searchValue, string type) {
-      Debug.Log($"Search triggered with value {searchValue} and {type}");
+      Debug.Log($"Search triggered with value '{searchValue}' and '{type}'");
+      if (type == "Object Name") {
+
+      } else if (type == "Component") {
+        Type systemType = Type.GetType(searchValue);
+        if (systemType != null) {
+          UnityEngine.Object[] types = GameObject.FindObjectsOfType(systemType);
+          Debug.Log($"[BTDebug Search] '{types.Length}' items found");
+        } else {
+          Debug.Log($"[BTDebug Search] Unknown type of '{searchValue}'. Please use a correct component type.");
+        }
+      } else {
+        Debug.Log("[BTDebug Search] Unknown search type of '{type}'");
+      }
     }
   } 
 }
